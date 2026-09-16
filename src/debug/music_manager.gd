84e4b2ba07ef82bcc0f95e4b159dcd7e52526b48
dcreasma <140018395+dcreasma@users.noreply.github.com>
2026-09-15 21:14:13 -0700

    Shop and Hub themes added along with MusicManager

---
+++ src/debug/music_manager.gd
@@ -0,0 +1,18 @@
+class_name MusicManager
+extends Node2D
+var songs ={}
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for child in get_children():
+		if child is AudioStreamPlayer2D:
+			songs.set(child.name, child)
+	print(songs)
+
+
+func stop_all_songs() -> void:
+	for song in songs.values():
+		song.stop()
+
+func switch_to_song(name: String) -> void:
+	stop_all_songs()
+	songs.get(name).play()
--- src/debug/shop.gd
+++ src/debug/shop.gd
@@ -2,6 +2,7 @@ class_name OMM_Shop
 extends Area2D
 @export var shop_menu: Menu
 @export var player: OMM_Player
+@export var music_manager: MusicManager
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	shop_menu.close()
@@ -14,9 +15,11 @@ func on_body_entered(body: Node2D) -> void:
 		enter_shop()
 		
 func enter_shop():
+		music_manager.switch_to_song("ShopTheme")
 		shop_menu.open()
 		player.shopping = true
 
 func exit_shop():
+	music_manager.switch_to_song("HubTheme")
 	shop_menu.close()
 	player.shopping = false
