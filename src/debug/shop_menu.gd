3119d96381f7c7cf14f62c69faa39e8245abae9b
dcreasma <140018395+dcreasma@users.noreply.github.com>
2026-09-19 23:39:37 -0700

    Revert "idk"
    This reverts commit 44df1f3c030df59086c3a875c82468e589dc2ea6.

--- src/debug/shop_menu.gd
+++ src/debug/shop_menu.gd
@@ -3,10 +3,6 @@ extends Menu
 
 @export var buyables: Array[OMM_ItemDefinition]
 @export var buy_buttons: Container
-@export var score_resource: OMM_ScoreResource
-@export var buy_sound: AudioStreamPlayer2D
-@export var not_enough_gold_sound: AudioStreamPlayer2D
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for buyable in buyables:
@@ -20,13 +16,6 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _on_buy_button_pressed(item_defintion: OMM_ItemDefinition):
-	if item_defintion.cost <= score_resource._score and !item_defintion.is_unlocked:
-		item_defintion.is_unlocked = true
-		score_resource.increment_score(-item_defintion.cost)
-		buy_sound.play()
-		print("bought")
-	else:
-		not_enough_gold_sound.play()
-		print("cant buy")
+	print(item_defintion.cost)
 	pass
 
