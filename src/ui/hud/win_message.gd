51d7c3a6ebe6b403c79188b58afbba09aab73be9
dcreasma <140018395+dcreasma@users.noreply.github.com>
2026-09-20 17:04:02 -0700

    On winning, goes to end screen after 5 seconds

--- src/ui/hud/win_message.gd
+++ src/ui/hud/win_message.gd
@@ -1,9 +1,16 @@
 extends Control
-
+@export var victory_timer: Timer
 func _ready():
+	victory_timer.timeout.connect(_to_end_screen)
 	StateManager.state_changed.connect(_on_state_changed)
+
 
 func _on_state_changed(state: StateManager.STATE, _old_state: StateManager.STATE):
 	if state == StateManager.STATE.WIN:
 		visible = true
+		victory_timer.start()
 	
+func _to_end_screen() -> void:
+	MusicManager.switch_to_song("ShopTheme")
+	get_tree().change_scene_to_file("res://src/ui/end_screen/end_screen.tscn")
+	pass
