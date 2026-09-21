07ef48558d6b374dbf190626a62a732536e80f81
Tim Creasman <tcreasman18@gmail.com>
2026-09-20 22:23:49 -0400

    Updated inf bomb price Updated half depth noise

--- src/resources/pure_noise.gd
+++ src/resources/pure_noise.gd
@@ -4,6 +4,7 @@ extends Resource
 static var rng = RandomNumberGenerator.new()
 
 static func get_noise_2dv(vector: Vector2i, percent_change = 0.1, _seed = 0) -> int:
+	randomize()
 	rng.set_seed(str(vector).hash() + _seed)
 	var random = rng.rand_weighted(PackedFloat32Array([1, percent_change]))
 	rng.state = 0
