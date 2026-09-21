670ae7302606e74f51dccb41e55adce15ebdfb93
dcreasma <140018395+dcreasma@users.noreply.github.com>
2026-09-20 17:14:10 -0700

    Update win_message.gd

--- src/ui/hud/win_message.gd
+++ src/ui/hud/win_message.gd
@@ -12,5 +12,6 @@ func _on_state_changed(state: StateManager.STATE, _old_state: StateManager.STATE
 	
 func _to_end_screen() -> void:
 	MusicManager.switch_to_song("ShopTheme")
+	
 	get_tree().change_scene_to_file("res://src/ui/end_screen/end_screen.tscn")
 	pass
